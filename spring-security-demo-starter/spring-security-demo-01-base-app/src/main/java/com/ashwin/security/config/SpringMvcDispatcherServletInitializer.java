626dd02b53b20b8321108f45e22d60289ce1833e
ashwinkarki <ashwin.karki123@yahoo.com>
2019-08-10 15:20:11 +0545

    doen upto inmemory

---
+++ spring-security-demo-starter/spring-security-demo-01-base-app/src/main/java/com/ashwin/security/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,23 @@
+package com.ashwin.security.config;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcDispatcherServletInitializer  extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	
+	 @Override
+	    protected Class <?> [] getRootConfigClasses() {
+	        // TODO Auto-generated method stub
+	        return null;
+	    }
+
+	    @Override
+	    protected Class <?> [] getServletConfigClasses() {
+	        return new Class[] { DemoAppConfig.class  };
+	    }
+
+	    @Override
+	    protected String[] getServletMappings() {
+	        return new String[] { "/" };
+	    }
+	
+}
---
+++ springsecurity2/src/main/java/com/ashwin/security/sconfig/SecurityWebApplicationInitilizer.java
@@ -0,0 +1,9 @@
+package com.ashwin.security.sconfig;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitilizer extends AbstractSecurityWebApplicationInitializer {
+	
+	
+
+}
